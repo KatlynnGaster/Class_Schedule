60c77da83d769b0a7d4c710539fd0280eb269346
TonyMontoyaO <amontoya.olivas15@gmail.com>
2025-03-22 13:23:44 -0600

    react-bootstrap was added and ViewSchedulePage component was added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import ViewSchedulePage from './ViewSchedulePage';
 
 
 function App() {
@@ -12,25 +13,7 @@ function App() {
   return (
     <>
       {/* for bootstrap*/}
-      <div className="App">
-            <h1 className="text-success">GeeksforGeeks</h1>
-            <div className="bg-info p-5">
-                <p className="">
-                    This is example for using button with
-                    bootstrap styling
-                </p>
-                <a
-                    className="btn btn-primary"
-                    data-bs-toggle="collapse"
-                    href="#collapseExample"
-                    role="button"
-                    aria-expanded="false"
-                    aria-controls="collapseExample"
-                >
-                    Bootstrap button
-                </a>
-            </div>
-        </div>
+      <ViewSchedulePage />
 
 
 
---
+++ src/ViewSchedulePage.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button'
+
+function ViewSchedulePage() {
+    return (
+        <div>
+            <h2>View Schedule Page Component works!</h2>
+        </div>
+
+    );
+}
+
+export default ViewSchedulePage;
