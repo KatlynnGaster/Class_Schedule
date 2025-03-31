fcddeed3898d174b9672cc51152f3c6dcb92f9a3
BergenCloninger <105687249+BergenCloninger@users.noreply.github.com>
2025-03-30 18:17:02 -0600

    Single word update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomePage from './homepage/App.jsx'
+import HomePage from './homepage/homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
